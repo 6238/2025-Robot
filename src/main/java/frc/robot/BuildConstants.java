2be00ba0d36817e53df35e31fc09058672020f96
KevinLprojects <125778924+KevinLprojects@users.noreply.github.com>
2025-01-26 16:14:31 -0800

    Swerve Updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 70;
-  public static final String GIT_SHA = "80ad60cb1910b2de699a48125a633f070e3d649b";
-  public static final String GIT_DATE = "2025-01-26 14:32:45 PST";
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "0cad1bfb5e4e31fe63d86c45bd8ccd96adc5a8ad";
+  public static final String GIT_DATE = "2025-01-26 14:56:32 PST";
   public static final String GIT_BRANCH = "kevin/intake";
-  public static final String BUILD_DATE = "2025-01-26 14:47:42 PST";
-  public static final long BUILD_UNIX_TIME = 1737931662657L;
+  public static final String BUILD_DATE = "2025-01-26 16:12:52 PST";
+  public static final long BUILD_UNIX_TIME = 1737936772810L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
