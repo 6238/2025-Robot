76316de95a705586fde4de93c172d1fd94295aff
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2025-02-15 05:30:24 -0800

    much nicer to read
    why was everything on one line? who knows

--- src/main/java/frc/robot/Subsystems/AlgaeEndEffectorSubsystem.java
+++ src/main/java/frc/robot/Subsystems/AlgaeEndEffectorSubsystem.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,6 +23,10 @@ public class AlgaeEndEffectorSubsystem extends SubsystemBase {
     final PositionVoltage p_request;
     final VelocityVoltage v_request;
 
+    boolean upToSpeed = false;
+    boolean velocityControl = false;
+    double speedSetpoint = 0.0;
+
     public AlgaeEndEffectorSubsystem() {
         leftMotor = new TalonFX(AlgaeEndEffector.LEFT_MOTOR_ID);
         rightMotor = new TalonFX(AlgaeEndEffector.RIGHT_MOTOR_ID);
@@ -27,13 +34,11 @@ public class AlgaeEndEffectorSubsystem extends SubsystemBase {
         var talonFXConfigs = new TalonFXConfiguration();
 
         var slot0Configs = talonFXConfigs.Slot0;
-
         slot0Configs.kP = 7.0; // An error of 1 rotation results in 2.4 V output
         slot0Configs.kI = 0.2; // no output for integrated error
         slot0Configs.kD = 0.2; // A velocity of 1 rps results in 0.1 V output
 
         var slot1Configs = talonFXConfigs.Slot1;
-
         slot1Configs.kS = 0.1; // Add 0.1 V output to overcome static friction
         slot1Configs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
         slot1Configs.kP = 0.2; // An error of 1 rps results in 0.11 V output
@@ -43,23 +48,21 @@ public class AlgaeEndEffectorSubsystem extends SubsystemBase {
         leftMotor.getConfigurator().apply(talonFXConfigs, 0.050);
         rightMotor.getConfigurator().apply(talonFXConfigs, 0.050);
 
+
         p_request = new PositionVoltage(0).withSlot(0);
         v_request = new VelocityVoltage(0).withSlot(1);
     }
-
-    boolean upToSpeed = false;
-    boolean velocityControl = false;
-    double speedSetpoint = 0.0;
-
+    // why does this work?
     public boolean motorStopped() {
-        // System.out.println("test");
-        return upToSpeed && (Math.abs(leftMotor.getVelocity().getValueAsDouble() + speedSetpoint) > (0.5 * speedSetpoint) || Math.abs(rightMotor.getVelocity().getValueAsDouble() - speedSetpoint) > (0.5 * speedSetpoint));
+        boolean left = Math.abs(leftMotor.getVelocity().getValueAsDouble() + speedSetpoint) > (0.5 * speedSetpoint);
+        boolean right = Math.abs(rightMotor.getVelocity().getValueAsDouble() - speedSetpoint) > (0.5 * speedSetpoint);
+        return upToSpeed && (left || right);
     }
 
     private void setMotorSpeed(double speed) {
-        upToSpeed = false;
         velocityControl = true;
         speedSetpoint = speed;
+        upToSpeed = isMotorsWithinSpeed(0.1);
         leftMotor.setControl(v_request.withVelocity(-speed));
         rightMotor.setControl(v_request.withVelocity(speed));
     }
@@ -105,14 +108,21 @@ public class AlgaeEndEffectorSubsystem extends SubsystemBase {
     public Command stopMotors() {
         return runOnce(() -> disableOutput());
     }
-
+    // todo: not grammatically correct
+    public boolean isMotorsWithinSpeed(double percentage) {
+        double maxError = speedSetpoint * percentage;
+        double leftError = Math.abs(leftMotor.getVelocity().getValueAsDouble() + speedSetpoint);
+        double rightError = Math.abs(rightMotor.getVelocity().getValueAsDouble() - speedSetpoint);
+        return leftError < maxError || rightError < maxError;
+    }
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("intakespeed", -leftMotor.getVelocity().getValueAsDouble());
+        upToSpeed = isMotorsWithinSpeed(0.1);
+
         SmartDashboard.putBoolean("intakeUpToSpeed", upToSpeed);
-        if (velocityControl && (Math.abs(leftMotor.getVelocity().getValueAsDouble() + speedSetpoint) < (0.1 * speedSetpoint) || Math.abs(rightMotor.getVelocity().getValueAsDouble() - speedSetpoint) < (0.1 * speedSetpoint))){
-            upToSpeed = true;
-            // System.err.println("up to speed");
-        }
+        SmartDashboard.putNumber("intakeTargetSpeed", speedSetpoint);
+        SmartDashboard.putNumber("intakeSpeedLeft", -leftMotor.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("intakeSpeedRight", rightMotor.getVelocity().getValueAsDouble());
+
     }
 }
