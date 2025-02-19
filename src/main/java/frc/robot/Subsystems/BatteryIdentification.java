a69e3269aecf47aba65fdc09aea7017721fe72e0
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-02-19 13:59:27 -0800

    Metadata

--- src/main/java/frc/robot/Subsystems/BatteryIdentification.java
+++ src/main/java/frc/robot/Subsystems/BatteryIdentification.java
@@ -1,28 +1,32 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.hal.SerialPortJNI;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.BuildConstants;
 
 public class BatteryIdentification extends SubsystemBase {
-    SerialPort serialPort;
-    boolean hasRead = false;
+  SerialPort serialPort;
+  boolean hasRead = false;
 
-    public BatteryIdentification() {
-        serialPort = new SerialPort(115200, Port.kUSB);
-        hasRead = false;
-    }
+  public BatteryIdentification() {
+    serialPort = new SerialPort(115200, Port.kUSB1);
+    hasRead = false;
+  }
 
-    public void readIfAvaliable() {
-        if (serialPort.getBytesReceived() >= 17 && !hasRead) { // BATTERY MESSAGE AVALIABLE
-            DataLogManager.log("BATTERY: " + serialPort.readString()); // LOG BATTERY NAME
-            hasRead = true;
-        }
+  public void readIfAvaliable() {
+    if (serialPort.getBytesReceived() >= 17 && !hasRead) { // BATTERY MESSAGE AVALIABLE
+      NetworkTableInstance.getDefault()
+        .getStringTopic("/Metadata/BATTERY_NAME")
+        .publish()
+        .set(serialPort.readString()); // LOG BATTERY NAME
+      hasRead = true;
     }
+  }
 
-    public void periodic() {
-        readIfAvaliable();
-    }
+  public void periodic() {
+    readIfAvaliable();
+  }
 }
